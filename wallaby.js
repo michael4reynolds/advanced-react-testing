396147bed9488a45551425e17a992f78e35ece4e
Michael Reynolds <michael4reynolds@gmail.com>
2016-12-19 21:58:54 -0500

    clean up with eslint

--- wallaby.js
+++ wallaby.js
@@ -1,19 +1,18 @@
 module.exports = function (wallaby) {
-
   return {
     files: ['src/**/*.js*', '!src/**/__tests__/*.js*'],
 
     tests: ['src/**/__tests__/*.js*'],
 
     compilers: {
-      'src/**/*.js*': wallaby.compilers.babel({})
+      'src/**/*.js*': wallaby.compilers.babel({}),
     },
 
     env: {
       type: 'node',
-      runner: 'node'
+      runner: 'node',
     },
 
-    testFramework: 'jest'
+    testFramework: 'jest',
   };
 };
