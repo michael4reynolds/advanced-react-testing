82a35d2f1e28824132a2db7c5a8d742e8e3bc127
Michael Reynolds <michael4reynolds@gmail.com>
2016-12-20 09:29:22 -0500

    use beforeEach to condense tests

--- src/components/__tests__/CommentBoxSpec.js
+++ src/components/__tests__/CommentBoxSpec.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import CommentBox from '../CommentBox';
 
-
 describe('CommentBox', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<CommentBox/>);
+  });
+
   it('has the correct class', () => {
-    const component = shallow(<CommentBox/>);
     expect(component.hasClass('comment-box')).toBe(true);
   });
 
   it('has a text area', () => {
-    const component = shallow(<CommentBox/>);
     expect(component.find('textarea').length).toBe(1);
   });
 
-
   it('has a button', () => {
-    const component = shallow(<CommentBox/>);
     expect(component.find('button').length).toBeTruthy();
   });
 });
