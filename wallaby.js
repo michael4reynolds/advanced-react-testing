9e4cd1a22c7b9705cbc77f23a4fe5607ded0bee0
Michael Reynolds <michael4reynolds@gmail.com>
2016-12-27 06:58:58 -0500

    eslint fix

--- wallaby.js
+++ wallaby.js
@@ -1,19 +1,19 @@
 module.exports = wallaby => ({
-    files: [
-      'src/**/*.js*',
-      '!src/**/__tests__/*.js*',
+  files: [
+    'src/**/*.js*',
+    '!src/**/__tests__/*.js*',
       { pattern: 'src/**/__tests__/**/*.snap' }],
 
-    tests: ['src/**/__tests__/*.js*'],
+  tests: ['src/**/__tests__/*.js*'],
 
-    compilers: {
-      'src/**/*.js*': wallaby.compilers.babel({}),
-    },
+  compilers: {
+    'src/**/*.js*': wallaby.compilers.babel({}),
+  },
 
-    env: {
-      type: 'node',
-      runner: 'node',
-    },
+  env: {
+    type: 'node',
+    runner: 'node',
+  },
 
   testFramework: 'jest',
 });
